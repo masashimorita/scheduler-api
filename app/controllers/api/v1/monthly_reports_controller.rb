86869924c22257711b7107483d2858fc69caa8af
MasaCode <masacode.vancouver@gmail.com>
2019-01-29 22:31:42 +0900

    :recycle: find records by date instead of time

--- app/controllers/api/v1/monthly_reports_controller.rb
+++ app/controllers/api/v1/monthly_reports_controller.rb
@@ -11,8 +11,8 @@ class Api::V1::MonthlyReportsController < Api::V1::ApiController
 
   def recalculate
     report = current_user.monthly_reports.find(report_params[:id])
-    beginning_of_month = Time.current.beginning_of_month
-    end_of_month = Time.current.end_of_month
+    beginning_of_month = Time.current.beginning_of_month.to_date
+    end_of_month = Time.current.end_of_month.to_date
     records = current_user.records.where("record_date >= ?", beginning_of_month).where("record_date <= ?", end_of_month).order(record_date: :asc)
     report.recalculate_all(records)
     json_response({report: report})
