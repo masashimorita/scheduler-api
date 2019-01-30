33270fc196514a6f814cfb009eb64eb0affa77cb
Masashi Morita <masashi.morita@opentone.co.jp>
2019-01-30 18:24:22 +0900

    :bug: 終了していないレコードを取得しないようにように修正

--- app/controllers/api/v1/monthly_reports_controller.rb
+++ app/controllers/api/v1/monthly_reports_controller.rb
@@ -13,7 +13,10 @@ class Api::V1::MonthlyReportsController < Api::V1::ApiController
     report = current_user.monthly_reports.find(report_params[:id])
     beginning_of_month = Time.current.beginning_of_month.to_date
     end_of_month = Time.current.end_of_month.to_date
-    records = current_user.records.where("record_date >= ?", beginning_of_month).where("record_date <= ?", end_of_month).order(record_date: :asc)
+    records = current_user.records.where("record_date >= ?", beginning_of_month)
+                                  .where("record_date <= ?", end_of_month)
+                                  .where("end_at IS NOT NULL")
+                                  .order(record_date: :asc)
     report.recalculate_all(records)
     json_response({report: report})
   end
