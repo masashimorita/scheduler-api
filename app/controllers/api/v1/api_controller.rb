d5f4b8f4499f40e4ff1d6244b7daf523e251a6d6
MasaCode <masacode.vancouver@gmail.com>
2019-02-01 22:28:43 +0900

    :bug: Adjust cors settings for client application

--- app/controllers/api/v1/api_controller.rb
+++ app/controllers/api/v1/api_controller.rb
@@ -4,10 +4,17 @@ class Api::V1::ApiController < ApplicationController
   include CustomValidationHelper
 
   attr_reader :current_user
+  before_action :set_headers
   before_action :authorize_request
 
   private
   def authorize_request
     @current_user = AuthorizeApiRequest.new(request.headers).call[:user]
+  end
+  def set_headers
+    headers['Access-Control-Allow-Origin'] = '*'
+    headers['Access-Control-Allow-Methods'] = 'POST, PUT, DELETE, GET, OPTIONS'
+    headers['Access-Control-Request-Method'] = '*'
+    headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
   end
 end
